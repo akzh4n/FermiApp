e30f9daa649c73e07f81483b0ebdb134dc55757c
manste1n <akzhan.kaz2003@gmail.com>
2022-07-02 21:22:21 +0600

    Fermi App

--- app/src/main/java/com/example/newfermiapp/topics/mechanicsgroup/Dynamics.java
+++ app/src/main/java/com/example/newfermiapp/topics/mechanicsgroup/Dynamics.java
@@ -2,21 +2,111 @@ package com.example.newfermiapp.topics.mechanicsgroup;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.example.newfermiapp.R;
 import com.example.newfermiapp.TopicsActivity;
+import com.example.newfermiapp.infomaterials.dynamics.dynamic1;
+import com.example.newfermiapp.infomaterials.dynamics.dynamic2;
+import com.example.newfermiapp.infomaterials.dynamics.dynamic3;
+import com.example.newfermiapp.infomaterials.dynamics.dynamic4;
+import com.example.newfermiapp.infomaterials.dynamics.dynamic5;
+import com.example.newfermiapp.infomaterials.dynamics.dynamic6;
+import com.example.newfermiapp.infomaterials.dynamics.dynamic7;
+import com.example.newfermiapp.infomaterials.dynamics.dynamic8;
+import com.example.newfermiapp.infomaterials.kinematics.kinemact1;
+import com.example.newfermiapp.infomaterials.kinematics.kinemact2;
+import com.example.newfermiapp.infomaterials.kinematics.kinemact3;
+import com.example.newfermiapp.infomaterials.kinematics.kinemact4;
+import com.example.newfermiapp.infomaterials.kinematics.kinemact5;
+import com.example.newfermiapp.infomaterials.kinematics.kinemact6;
+import com.example.newfermiapp.infomaterials.kinematics.kinemact7;
+import com.example.newfermiapp.infomaterials.kinematics.kinemact8;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Dynamics extends AppCompatActivity {
+
+    private CardView card1, card2, card3, card4, card5, card6, card7, card8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dynamics);
+
+        card1 = (CardView) findViewById(R.id.dycard1);
+        card2 = (CardView) findViewById(R.id.dycard2);
+        card3 = (CardView) findViewById(R.id.dycard3);
+        card4 = (CardView) findViewById(R.id.dycard4);
+        card5 = (CardView) findViewById(R.id.dycard5);
+        card6 = (CardView) findViewById(R.id.dycard6);
+        card7 = (CardView) findViewById(R.id.dycard7);
+        card8 = (CardView) findViewById(R.id.dycard8);
+
+
+        card1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), dynamic1.class));
+            }
+        });
+
+        card2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), dynamic2.class));
+            }
+        });
+
+        card3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), dynamic3.class));
+            }
+        });
+
+
+        card4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), dynamic4.class));
+            }
+        });
+
+
+        card5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), dynamic5.class));
+            }
+        });
+
+
+        card6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), dynamic6.class));
+            }
+        });
+
+
+        card7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), dynamic7.class));
+            }
+        });
+
+        card8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), dynamic8.class));
+            }
+        });
 
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation3);
--- app/src/main/java/com/example/newfermiapp/topics/mechanicsgroup/WE.java
+++ app/src/main/java/com/example/newfermiapp/topics/mechanicsgroup/WE.java
@@ -12,12 +12,19 @@ import android.view.View;
 import com.example.newfermiapp.R;
 import com.example.newfermiapp.TopicsActivity;
 import com.example.newfermiapp.infomaterials.we.weact1;
+import com.example.newfermiapp.infomaterials.we.weact2;
+import com.example.newfermiapp.infomaterials.we.weact3;
+import com.example.newfermiapp.infomaterials.we.weact4;
+import com.example.newfermiapp.infomaterials.we.weact5;
+import com.example.newfermiapp.infomaterials.we.weact6;
+import com.example.newfermiapp.infomaterials.we.weact7;
+import com.example.newfermiapp.infomaterials.we.weact8;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class WE extends AppCompatActivity {
 
 
-    private CardView card1;
+    private CardView card1, card2, card3, card4, card5, card6, card7, card8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +33,62 @@ public class WE extends AppCompatActivity {
 
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation3);
-        card1 = (CardView) findViewById(R.id.kinemcard1) ;
+        card1 = (CardView) findViewById(R.id.wecard1) ;
+        card2 = (CardView)  findViewById(R.id.wecard2);
+        card3 = (CardView) findViewById(R.id.wecard3) ;
+        card4 = (CardView)  findViewById(R.id.wecard4);
+        card5 = (CardView) findViewById(R.id.wecard5) ;
+        card6 = (CardView)  findViewById(R.id.wecard6);
+        card7 = (CardView) findViewById(R.id.wecard7) ;
+        card8 = (CardView)  findViewById(R.id.wecard8);
 
 
         card1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(), weact1.class));
+            }
+        });
+        card2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), weact2.class));
+            }
+        });
+        card3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), weact3.class));
+            }
+        });
+        card4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), weact4.class));
+            }
+        });
+        card5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), weact5.class));
+            }
+        });
+        card6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), weact6.class));
+            }
+        });
+        card7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), weact7.class));
+            }
+        });
+        card8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), weact8.class));
             }
         });
 
